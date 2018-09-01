63bf16d151d4bbf5206a7038cd0e75bfe99c0582
lori227 <lori227@qq.com>
2018-09-01 12:01:41 +0800

    kfresult

--- Server/KFCommon/KFResult.h
+++ Server/KFCommon/KFResult.h
@@ -57,6 +57,54 @@ namespace KFrame
 
     ///////////////////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////////
+    template< class T >
+    class KFResultQueue
+    {
+    public:
+        KFResultQueue() = default;
+        ~KFResultQueue()
+        {
+            for ( auto kfresult : _idle_list )
+            {
+                __KF_DESTROY__( KFResult<T>, kfresult );
+            }
+            _idle_list.clear();
+
+            for ( auto kfresult : _use_list )
+            {
+                __KF_DESTROY__( KFResult<T>, kfresult );
+            }
+            _use_list.clear();
+        }
+
+        // 申请
+        KFResult< T >* Alloc()
+        {
+            KFResult< T >* kfresult = nullptr;
+            if ( _idle_list.empty() )
+            {
+                kfresult = __KF_CREATE_BATCH__( KFResult< T >, 10 );
+            }
+            else
+            {
+                kfresult = _idle_list.front();
+                _idle_list.pop_front();
+            }
+
+            _use_list.push_back( kfresult );
+            return kfresult;
+        }
+
+        // 释放
+        void
+
+    private:
+        // 空闲的列表
+        std::list < KFResult< T >* > _idle_list;
+
+        // 使用中的列表
+        std::list < KFResult< T >* > _use_list;
+    };
 }
 
 #endif
