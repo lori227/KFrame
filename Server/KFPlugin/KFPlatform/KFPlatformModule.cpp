05c7ed24255bb68bfa97ba3afd52bf6a5bcb0598
lori227 <lori227@qq.com>
2018-12-21 10:09:56 +0800

    setting/startup

--- Server/KFPlugin/KFPlatform/KFPlatformModule.cpp
+++ Server/KFPlugin/KFPlatform/KFPlatformModule.cpp
@@ -1,20 +1,19 @@
 ﻿#include "KFPlatformModule.h"
 #include "KFHttp/KFHttpURL.h"
+#include "KFPlatformConfig.h"
 
 namespace KFrame
 {
     const std::string& KFPlatformModule::GetPlatformApiUrl()
     {
-        static auto kfoption = _kf_option->FindOption( __KF_STRING__( platapiurl ) );
-        return kfoption->_str_value;
+        return _kf_platform_config->_app_url;
     }
 
     const std::string& KFPlatformModule::MakePlatformSign( uint32 nowtime )
     {
         static std::string _sign = "";
-        static auto _app_key = _kf_option->GetString( __KF_STRING__( platappkey ) );
 
-        auto source = __FORMAT__( "{}{}", _app_key, nowtime );
+        auto source = __FORMAT__( "{}{}", _kf_platform_config->_app_key, nowtime );
         _sign = KFCrypto::Md5Encode( source );
 
         return _sign;
@@ -22,16 +21,13 @@ namespace KFrame
 
     const std::string& KFPlatformModule::MakePlatformUrl( const std::string& path )
     {
-        static auto _api_url = GetPlatformApiUrl();
-        static auto _app_id = _kf_option->GetString( __KF_STRING__( platappid ) );
-
         MapString params;
-        params[ "appid" ] = _app_id;
+        params[ "appid" ] = _kf_platform_config->_app_id;
         params[ "timestamp" ] = KFUtility::ToString( KFGlobal::Instance()->_real_time );
         params[ "sig" ] = MakePlatformSign( KFGlobal::Instance()->_real_time );
 
         static std::string _total_url = "";
-        _total_url = _api_url + path + KFHttpURL::EncodeParams( params );
+        _total_url = _kf_platform_config->_app_url + path + KFHttpURL::EncodeParams( params );
         return _total_url;
     }
 
