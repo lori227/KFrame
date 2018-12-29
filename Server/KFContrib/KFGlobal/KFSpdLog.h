9cd890ae4f427a4d88166f571f9e6316a9774b27
lori227 <lori227@qq.com>
2018-12-29 15:00:58 +0800

    优化本地log

--- Server/KFContrib/KFGlobal/KFSpdLog.h
+++ Server/KFContrib/KFGlobal/KFSpdLog.h
@@ -21,14 +21,13 @@ namespace KFrame
         void Log( uint32 loglevel, const std::string& content );
 
     protected:
-        const spdlogger& GetLogger();
 
-        const spdlogger& CreateLogger();
-        std::string GetLoggerName();
+        // 创建log
+        void CreateLogger( const std::string& path, const std::string& appname, const std::string& apptype, const std::string& strappid );
 
     private:
-        std::string _local_log_path;
-        std::map<std::string, spdlogger> _local_loggers;
+        // 日志对象
+        spdlogger _logger;
     };
 
 }
