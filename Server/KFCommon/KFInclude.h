b52f9a38c0487aa04ee14d8ecc9f16ae68d450b3
lori227 <lori227@qq.com>
2019-01-06 11:33:05 +0800

    Update KFInclude.h

--- Server/KFCommon/KFInclude.h
+++ Server/KFCommon/KFInclude.h
@@ -27,12 +27,12 @@
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
 typedef void*	voidptr;
-typedef int8_t	int8;
+typedef char	int8;
 typedef int16_t	int16;
 typedef int32_t	int32;
 typedef int64_t	int64;
 
-typedef uint8_t  uint8;
+typedef unsigned char  uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
