660efc21bc16c8ca505162b9eb61a781756563db
lori227 <lori227@qq.com>
2019-01-26 09:52:14 +0800

    Update frame-update.bat

--- 1.Server/2.Midware/frame-update.bat
+++ 1.Server/2.Midware/frame-update.bat
@@ -5,5 +5,17 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 rem ===========================================================================
 set framepath=..\9.Template
 set localpath=.
+
+mkdir %localpath%\_bin\setting1
+xcopy /y /S %localpath%\_bin\setting\* %localpath%\_bin\setting1
+mkdir %localpath%\_bin\_gcm\conf_input1
+xcopy /y /S %localpath%\_bin\_gcm\conf_input\* %localpath%\_bin\_gcm\conf_input1
+
 xcopy /y /S %framepath%\* %localpath%\
+
+xcopy /y /S %localpath%\_bin\setting1\* %localpath%\_bin\setting\
+rd /Q /S %localpath%\_bin\setting1
+xcopy /y /S %localpath%\_bin\_gcm\conf_input1\* %localpath%\_bin\_gcm\conf_input\
+rd /Q /S %localpath%\_bin\_gcm\conf_input1
+
 rem ===========================================================================
