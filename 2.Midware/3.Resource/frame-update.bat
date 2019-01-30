a8e08884a598cfd87663a39987226051704aaa78
lori227 <lori227@qq.com>
2019-01-30 12:49:48 +0800

    resource

--- 2.Midware/3.Resource/frame-update.bat
+++ 2.Midware/3.Resource/frame-update.bat
@@ -13,6 +13,9 @@ xcopy /y /S %localpath%\_bin\_gcm\conf_input\* %localpath%\_bin\_gcm\conf_input1
 
 copy /y %localpath%\KFCommon\KFField.h %localpath%\KFCommon\KFField.h1
 
+mkdir %localpath%\_excel1
+del %localpath%\_excel\_parse.exe
+xcopy /y /S %localpath%\_excel\* %localpath%\_excel1
 rem ===========================================================================
 rem ===========================================================================
 rem ===========================================================================
@@ -31,5 +34,8 @@ rd /Q /S %localpath%\_bin\_gcm\conf_input1
 
 copy /y %localpath%\KFCommon\KFField.h1 %localpath%\KFCommon\KFField.h
 del %localpath%\KFCommon\KFField.h1
+
+xcopy /y /S %localpath%\_excel1\* %localpath%\_excel
+rd /Q /S %localpath%\_excel1
 
 rem ===========================================================================
