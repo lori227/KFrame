120352c771a84e83d08d511039240b7c1e532636
lori227 <lori227@qq.com>
2019-02-13 13:31:24 +0800

    shell

--- 2.Midware/1.Server/_build/linux/build_debug.sh
+++ 2.Midware/1.Server/_build/linux/build_debug.sh
@@ -1,5 +1,7 @@
-mkdir -p ../../../Bin/lib
-mkdir -p ../../../Bin/bin
+mkdir -p ../../_bin/lib
+mkdir -p ../../_bin/bin/debug
+
+cp -a -f ../../../../1.Frame/9.Template/1.Server/_bin/bin/debug ../../_bin/bin/
 
 cd ../../../Resource/protocol
 sh linux.sh
@@ -7,4 +9,4 @@ cd -
 
 cmake ../../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
 make -j 6
-cp -a -f ../../_lib/linux/3rd/*.so* ../../../Bin/lib
+cp -a -f ../../_lib/linux/3rd/*.so* ../../_bin/lib
--- 2.Midware/1.Server/_build/linux/build_release.sh
+++ 2.Midware/1.Server/_build/linux/build_release.sh
@@ -1,5 +1,7 @@
-mkdir -p ../../../Bin/lib
-mkdir -p ../../../Bin/bin
+mkdir -p ../../_bin/lib
+mkdir -p ../../_bin/bin/release
+
+cp -a -f ../../../../1.Frame/9.Template/1.Server/_bin/bin/release ../../_bin/bin/
 
 cd ../../../Resource/protocol
 sh linux.sh
@@ -7,4 +9,4 @@ cd -
 
 cmake ../../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release 
 make -j 6
-cp -a -f ../../_lib/linux/3rd/*.so* ../../../Bin/lib
+cp -a -f ../../_lib/linux/3rd/*.so* ../../_bin/lib
