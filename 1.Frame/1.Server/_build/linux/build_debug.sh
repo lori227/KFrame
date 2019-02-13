b613fc18914cacaba7bc1410bb3a2a48bd662b05
lori227 <lori227@qq.com>
2019-02-13 13:47:43 +0800

    shell

--- 1.Frame/1.Server/_build/linux/build_debug.sh
+++ 1.Frame/1.Server/_build/linux/build_debug.sh
@@ -14,5 +14,5 @@ mkdir -p ../../../9.Template/1.Server/_bin/bin/debug
 
 cp -a -f ../../_lib/linux/3rd/*.so* ../../_bin/lib/
 cp -a -f ../../_lib/linux/*.a ../../../9.Template/1.Server/_lib/linux/
-cp -a -f ../../_lib/linux/3rd/*.so* ../../../9.Template/1.Server/_lib/linux/3rd/
+cp -a -f ../../_lib/linux/3rd/* ../../../9.Template/1.Server/_lib/linux/3rd/
 cp -a -f ../../_bin/bin/debug ../../../9.Template/1.Server/_bin/bin/
--- 1.Frame/1.Server/_build/linux/build_release.sh
+++ 1.Frame/1.Server/_build/linux/build_release.sh
@@ -12,5 +12,5 @@ mkdir -p ../../../9.Template/1.Server/_bin/bin/release
 
 cp -a -f ../../_lib/linux/3rd/*.so* ../../_bin/lib/
 cp -a -f ../../_lib/linux/*.a ../../../9.Template/1.Server/_lib/linux/
-cp -a -f ../../_lib/linux/3rd/*.so* ../../../9.Template/1.Server/_lib/linux/3rd/
+cp -a -f ../../_lib/linux/3rd/* ../../../9.Template/1.Server/_lib/linux/3rd/
 cp -a -f ../../_bin/bin/release ../../../9.Template/1.Server/_bin/bin/
