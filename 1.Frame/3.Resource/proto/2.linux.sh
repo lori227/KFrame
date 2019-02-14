541d57c644e66b7b685a00abb3864744c46b7619
lori227 <lori227@qq.com>
2019-02-14 10:21:33 +0800

    Update 2.linux.sh

--- 1.Frame/3.Resource/proto/2.linux.sh
+++ 1.Frame/3.Resource/proto/2.linux.sh
@@ -1,13 +1,13 @@
 #! /bin/sh 
 
-chmod 777 __protoc
+chmod 777 4.protoc
 export LD_LIBRARY_PATH=/usr/local/lib
 
 for file in `ls *.proto`  
 do  
   echo $file  
-  bash 4.protoc -I=./ --cpp_out=dllexport_decl=LIBPROTOC_EXPORT:./ $file
+./4.protoc -I=./ --cpp_out=dllexport_decl=LIBPROTOC_EXPORT:./ $file
 done  
 
-\cp *.pb.* ../KFContrib/KFProtocol/
+\cp *.pb.* ../../1.Server/KFContrib/KFProtocol/
 rm -f *.pb.*
