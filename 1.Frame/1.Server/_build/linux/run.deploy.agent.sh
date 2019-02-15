72ab5a78a7205637c2bf4a11048eb0597adaf6e5
lori227 <lori227@qq.com>
2019-02-15 14:11:43 +0800

    agent

--- 1.Frame/1.Server/_build/linux/run.deploy.agent.sh
+++ 1.Frame/1.Server/_build/linux/run.deploy.agent.sh
@@ -10,4 +10,4 @@ if [ ! -n "$2" ] ;then
 fi
 
 cd /data/deploy
-./bin/kfdeploy appname=deploy apptype=agent appid=$1.12.0.1 log=1 service=1.$2 deploy=$1.11.0.1|172.31.192.96|10000
+./bin/kfdeploy appname=deploy apptype=agent appid=$1.12.0.1 log=1 service=1.$2 deploy="$1.11.0.1|172.31.192.96|10000"
