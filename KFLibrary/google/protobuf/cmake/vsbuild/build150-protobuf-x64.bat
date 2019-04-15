4b88fb75a078546a63439cb241c46623307a189d
lori227 <lori227@qq.com>
2019-04-15 16:43:04 +0800

    整理protobuf编译

--- KFLibrary/google/protobuf/cmake/vsbuild/build150-protobuf-x64.bat
+++ KFLibrary/google/protobuf/cmake/vsbuild/build150-protobuf-x64.bat
@@ -2,8 +2,8 @@
 :  build protobuf
 
 "%VS150COMNTOOLS%..\IDE\Devenv" protobuf.sln /build "Debug|X64" /project libprotobuf
-copy /y .\x64\Debug\libprotobufd.lib ..\..\..\..\..\_lib\win64\debug\3rd\
+copy /y .\Debug\libprotobufd.lib ..\..\..\..\..\_lib\win64\debug\3rd\
 
 
 "%VS150COMNTOOLS%..\IDE\Devenv" protobuf.sln /build "Release|X64" /project libprotobuf
-copy /y .\x64\Release\libprotobuf.lib ..\..\..\..\..\_lib\win64\release\3rd\
+copy /y .\Release\libprotobuf.lib ..\..\..\..\..\_lib\win64\release\3rd\
