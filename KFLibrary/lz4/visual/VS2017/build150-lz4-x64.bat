670e82f3bfd67e10007fdd4742ecce492c5d5a1d
lori227 <lori227@qq.com>
2019-06-22 17:38:21 +0800

    lz4

---
+++ KFLibrary/lz4/visual/VS2017/build150-lz4-x64.bat
@@ -0,0 +1,10 @@
+
+:  build zlib
+
+
+"%VS150COMNTOOLS%..\IDE\Devenv" lz4.sln /build "Debug|X64" /project liblz4
+copy /y .\bin\x64_Debug\liblz4_static.lib ..\..\..\..\_lib\win64\debug\3rd\liblz4d.lib
+
+
+"%VS150COMNTOOLS%..\IDE\Devenv" lz4.sln /build "Release|X64" /project liblz4
+copy /y .\bin\x64_Release\liblz4_static.lib ..\..\..\..\_lib\win64\release\3rd\liblz4.lib
