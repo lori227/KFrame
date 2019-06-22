7c3092594081dd6a0101c92e4f16a702af41b81d
lori227 <lori227@qq.com>
2019-06-22 18:34:36 +0800

    lz4

--- KFContrib/KFUtility/KFCompress.cpp
+++ KFContrib/KFUtility/KFCompress.cpp
@@ -1,10 +1,14 @@
 ﻿#include "KFrame.h"
 #include "KFCompress.h"
-#include "zlib/zlib.h"
 #include "KFDecode.h"
+#include "lz4/lib/lz4.h"
 
 namespace KFrame
 {
+    static uint32 _zib_length = KFBufferEnum::Buff_20M;
+    static uint32 _data_length = KFBufferEnum::Buff_40M;
+    static uint32 _compress_level = 1;
+
     int32 KFCompress::Zib( const std::string& value, std::string& result )
     {
         result.clear();
@@ -13,53 +17,19 @@ namespace KFrame
             return 0;
         }
 
-
-        uLongf ziplength = KFBufferEnum::Buff_20M;
-        auto zipbuffer = __KF_UINT8__( ziplength );
-
         // 压缩数据
-        // uLongf compresslength = 0;
-        auto recode = compress( zipbuffer, &ziplength, reinterpret_cast< const uint8* >( value.data() ), static_cast< uLong >( value.size() ) );
-        if ( recode == Z_OK )
+        auto zibbuffer = __KF_UINT8__( _zib_length );
+        auto ziblength = LZ4_compress_fast( value.data(), ( int8* )zibbuffer, value.size(), _zib_length, _compress_level );
+        if ( ziblength > 0 )
         {
-            result.assign( reinterpret_cast< int8* >( zipbuffer ), ziplength );
+            result.assign( ( int8* )zibbuffer, ziblength );
         }
         else
         {
-            __LOG_ERROR__( "compress failed=[{}]", recode );
+            __LOG_ERROR__( "compress failed=[{}]", ziblength );
         }
 
-        return recode;
-    }
-
-    int32 KFCompress::Compress( const std::string& value, std::string& result )
-    {
-        result.clear();
-        if ( value.empty() )
-        {
-            return 0;
-        }
-
-        uLongf ziplength = KFBufferEnum::Buff_20M;
-        auto zipbuffer = __KF_UINT8__( ziplength );
-
-        // 压缩数据
-        auto recode = compress( zipbuffer, &ziplength, reinterpret_cast< const uint8* >( value.data() ), static_cast< uLong >( value.size() ) );
-        if ( recode == Z_OK )
-        {
-            uint32 maxlength = KFBufferEnum::Buff_40M;
-            auto savebuffer = __KF_INT8__( maxlength );
-
-            // 转为可视字符串
-            auto savelength = KFDecode::UByteToString( zipbuffer, ziplength, savebuffer, maxlength );
-            result.assign( savebuffer, savelength );
-        }
-        else
-        {
-            __LOG_ERROR__( "compress failed=[{}]", recode );
-        }
-
-        return recode;
+        return ziblength;
     }
 
     int32 KFCompress::UnZib( const std::string& value, std::string& result )
@@ -70,21 +40,47 @@ namespace KFrame
             return 0;
         }
 
-        uLongf ziplength = KFBufferEnum::Buff_20M;
-        auto zipbuffer = __KF_UINT8__( ziplength );
-
         // 解压缩
-        auto recode = uncompress( zipbuffer, &ziplength, reinterpret_cast< const uint8* >( value.c_str() ), static_cast< uLong >( value.size() ) );
-        if ( recode == Z_OK )
+        auto zibbuffer = __KF_UINT8__( _zib_length );
+        auto ziblength = LZ4_decompress_safe( value.data(), ( int8* )zibbuffer, value.size(), _zib_length );
+        if ( ziblength > 0 )
         {
-            result.assign( reinterpret_cast< int8* >( zipbuffer ), ziplength );
+            result.assign( ( int8* )zibbuffer, ziblength );
         }
         else
         {
-            __LOG_ERROR__( "uncompress failed=[{}]", recode );
+            __LOG_ERROR__( "uncompress failed=[{}]", ziblength );
         }
 
-        return recode;
+        return ziblength;
+    }
+
+
+    int32 KFCompress::Compress( const std::string& value, std::string& result )
+    {
+        result.clear();
+        if ( value.empty() )
+        {
+            return 0;
+        }
+
+        // 压缩数据
+        auto zibbuffer = __KF_UINT8__( _zib_length );
+        auto length = LZ4_compress_fast( value.data(), ( int8* )zibbuffer, value.size(), _zib_length, _compress_level );
+        if ( length > 0 )
+        {
+            auto databuffer = __KF_INT8__( _data_length );
+
+            // 转为可视字符串
+            auto datalength = KFDecode::UByteToString( zibbuffer, length, databuffer, _data_length );
+            result.assign( databuffer, datalength );
+        }
+        else
+        {
+            __LOG_ERROR__( "compress failed=[{}]", length );
+        }
+
+        return length;
     }
 
     int32 KFCompress::UnCompress( const std::string& value, std::string& result )
@@ -95,26 +91,22 @@ namespace KFrame
             return 0;
         }
 
-        auto ziplength = KFBufferEnum::Buff_20M;
-        auto zipbuffer = __KF_UINT8__( ziplength );
-
         // 转成压缩数据
-        auto length = KFDecode::StringToUByte( value.data(), static_cast< uint32 >( value.size() ), zipbuffer, ziplength );
-
-        uLongf maxlength = KFBufferEnum::Buff_40M;
-        auto resultbuffer = __KF_INT8__( maxlength );
+        auto zibbuffer = __KF_UINT8__( _zib_length );
+        auto ziblength = KFDecode::StringToUByte( value.data(), ( uint32 )value.size(), zibbuffer, _zib_length );
 
         // 解压缩
-        auto recode = uncompress( reinterpret_cast<Bytef*>( resultbuffer ), &maxlength, zipbuffer, length );
-        if ( recode == Z_OK )
+        auto databuffer = __KF_INT8__( _data_length );
+        auto datalength = LZ4_decompress_safe( ( int8* )zibbuffer, databuffer, ziblength, _data_length );
+        if ( datalength > 0 )
         {
-            result.assign( reinterpret_cast< int8* >( resultbuffer ), maxlength );
+            result.assign( databuffer, datalength );
         }
         else
         {
-            __LOG_ERROR__( "uncompress failed=[{}]", recode );
+            __LOG_ERROR__( "uncompress failed=[{}]", datalength );
         }
 
-        return recode;
+        return datalength;
     }
 }
