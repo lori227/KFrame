4ed5d1e01afe75d088bf934c748ab2a39a476e35
lori227 <lori227@qq.com>
2019-07-03 16:18:03 +0800

    library build

--- KFLibrary/zlib/contrib/vstudio/vc11/build150-zlib-x64.bat
+++ KFLibrary/zlib/contrib/vstudio/vc11/build150-zlib-x64.bat
@@ -2,9 +2,9 @@
 :  build zlib
 
 
-"%VS150COMNTOOLS%..\IDE\Devenv" zlibvc.sln /build "Debug|X64"
+"%VS150COMNTOOLS%..\IDE\Devenv" zlibvc.sln /build "Debug|X64" /project zlibstat
 copy /y .\x64\ZlibStatDebug\zlibstat.lib ..\..\..\..\..\_lib\win64\debug\3rd\zlibd.lib
 
 
-"%VS150COMNTOOLS%..\IDE\Devenv" zlibvc.sln /build "Release|X64"
+"%VS150COMNTOOLS%..\IDE\Devenv" zlibvc.sln /build "Release|X64" /project zlibstat
 copy /y .\x64\ZlibStatRelease\zlibstat.lib ..\..\..\..\..\_lib\win64\release\3rd\zlib.lib
