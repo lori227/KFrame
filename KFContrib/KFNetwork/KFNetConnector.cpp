e63d1a420228bfcb504f2e3daec93fd36972a956
lori227 <lori227@qq.com>
2020-04-28 20:57:32 +0800

    Update KFNetConnector.cpp

--- KFContrib/KFNetwork/KFNetConnector.cpp
+++ KFContrib/KFNetwork/KFNetConnector.cpp
@@ -86,7 +86,8 @@ namespace KFrame
                     _last_recv_time = _net_services->_now_time;
 
                     // 断线处理
-                    // _net_services->SendEventToServices( this, KFNetDefine::DisconnectEvent );
+                    _net_services->SendEventToServices( this, KFNetDefine::DisconnectEvent );
+                    __LOG_DEBUG__( "net connector=[{}:{}] message timeout", _session_id, _object_id );
                 }
             }
         }
