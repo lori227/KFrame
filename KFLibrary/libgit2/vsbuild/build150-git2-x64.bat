85f41052fcea1c16ee308f59b0180b2a04ba9ba7
lori227 <lori227@qq.com>
2020-11-30 13:14:02 +0800

    git build

---
+++ KFLibrary/libgit2/vsbuild/build150-git2-x64.bat
@@ -0,0 +1,9 @@
+﻿
+:  build libuv
+
+"%VS150COMNTOOLS%..\IDE\Devenv" libgit2.sln /rebuild "Debug|X64" /project git2
+copy /y .\Debug\git2.lib ..\..\..\_lib\win64\debug\3rd\git2d.lib
+
+
+"%VS150COMNTOOLS%..\IDE\Devenv" libgit2.sln /rebuild "Release|X64" /project git2
+copy /y .\Release\git2.lib ..\..\..\_lib\win64\release\3rd\git2.lib
--- _build/win64/build150-library-x64.bat
+++ _build/win64/build150-library-x64.bat
@@ -82,14 +82,7 @@ cd ../../
 :  build libgit2
 
 cd libgit2/vsbuild
-
-"%VS150COMNTOOLS%..\IDE\Devenv" libgit2.sln /rebuild "Debug|X64" /project git2
-copy /y .\Debug\git2.lib ..\..\..\_lib\win64\debug\3rd\git2d.lib
-
-
-"%VS150COMNTOOLS%..\IDE\Devenv" libgit2.sln /rebuild "Release|X64" /project git2
-copy /y .\Release\git2.lib ..\..\..\_lib\win64\release\3rd\git2.lib
-
+call build150-git2-x64.bat
 cd ../../
 :================================================================
 
