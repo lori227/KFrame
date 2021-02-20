2883ff8bd71ed144b8924d00c5b9fc29e4b1a7ca
liuxiaobing <liuxiaobing@xd.com>
2021-02-20 11:29:52 +0800

    Update KFThread.cpp

--- KFContrib/KFUtility/KFThread.cpp
+++ KFContrib/KFUtility/KFThread.cpp
@@ -4,13 +4,8 @@ namespace KFrame
 {
     uint32 KFThread::GetThreadID()
     {
-#if __KF_SYSTEM__ == __KF_WIN__
         uint32 threadid = Poco::Thread::currentTid();
         return threadid;
-#else
-        auto thread = Poco::Thread::currentTid();
-        return thread->__sig;
-#endif
     }
 
     uint32 KFThread::CreateThread( KFThreadFunction& function, const char* file, uint32 line )
